f7654428dfd6d755cea25a414592aac8c71a71e6
Felipe Albrecht <felipe.albrecht@gmail.com>
2018-03-06 12:40:32 +0900

    Fix POST requests

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ var compression = require('compression')
 var proxy = require('http-proxy-middleware');
 
 var app = express();
+
+
+MIDDLEWARE_URL = "http://localhost:56572"
+var options = {
+  target: MIDDLEWARE_URL,
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api': ''
+  }
+};
+app.use('/api', proxy(options));
+
+
 app.use(compression());
 
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -16,23 +29,15 @@ app.use(express.static(path.join(__dirname, 'dist')));
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-MIDDLEWARE_URL = "http://localhost:56572"
-var options = {
-  target: MIDDLEWARE_URL,
-  changeOrigin: true,
-  pathRewrite: {
-      '^/api' : ''
-  }
-};
-app.use('/api', proxy(options));
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -40,7 +45,7 @@ app.use(function(req, res, next) {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500).send({
     message: err.message,
     error: err
