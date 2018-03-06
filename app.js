029f2e31990259ed15bc9203cdbd4a27c472d801
Felipe Albrecht <felipe.albrecht@gmail.com>
2018-03-06 12:20:02 +0900

    Proxy api requests to the middlewaare

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var compression = require('compression')
+var proxy = require('http-proxy-middleware');
 
 var app = express();
 app.use(compression());
 
 app.use(express.static(path.join(__dirname, 'dist')));
-
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -19,6 +19,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+MIDDLEWARE_URL = "http://localhost:56572"
+var options = {
+  target: MIDDLEWARE_URL,
+  changeOrigin: true,
+  pathRewrite: {
+      '^/api' : ''
+  }
+};
+app.use('/api', proxy(options));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
